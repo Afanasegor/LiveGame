c62c56dbbcdebf05ffc2355ac28ef61fcbe34868
Afanasegor <afanasegor3@mail.ru>
2021-02-15 15:33:42 +0300

    First commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Live;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(0, 0);
+
+            GameEngine gameEngine = new GameEngine
+                (
+                    rows: 100,
+                    cols: 300,
+                    density: 2
+                );
+
+            while (true)
+            {
+                Console.Title = gameEngine.CurrentGeneration.ToString();
+
+                bool[,] field = gameEngine.GetCurrentGeneration();
+
+                for (int y = 0; y < field.GetLength(1); y++)
+                {
+                    char[] str = new char[field.GetLength(0)];
+                    for (int x = 0; x < field.GetLength(0); x++)
+                    {
+                        if (field[x, y])
+                            str[x] = '#';
+                        else
+                            str[x] = ' ';
+                    }
+                    Console.WriteLine(str);
+                }
+                Console.SetCursorPosition(0,0);
+                gameEngine.NextGeneration();
+            }
+        }
+    }
+}
